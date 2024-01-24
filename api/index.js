624e9a436a3127216e92212a342513ab08cdbdc8
HMertYAVAS <hmertyavas@hotmail.com>
2024-01-24 22:52:03 +0300

    Signup and save api (bcryptjs for hashedPass)

--- api/index.js
+++ api/index.js
@@ -2,10 +2,13 @@ import express from "express";
 import mongoose from "mongoose";
 import dotenv from 'dotenv'
 import userRoutes from './routes/user.route.js'
+import signupRoutes from './routes/auth.route.js'
+
 
 dotenv.config();
 
 const app = express();
+
 
 mongoose
   .connect(
@@ -24,4 +27,7 @@ app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
 
+app.use(express.json());
+
 app.use('/testAPI',userRoutes);
+app.use('/testAPI',signupRoutes);

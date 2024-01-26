d84c2ac4212c758c783781e8c09cb07a94ce2823
HMertYAVAS <hmertyavas@hotmail.com>
2024-01-26 19:15:48 +0300

    PrivateRoutes; Dashboard,SignIn,SignUp

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,26 +1,30 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import About from "./pages/About";
-import Projects from "./pages/Projects"
-import Dashboard from "./pages/Dashboard"
-import SignIn from "./pages/SignIn"
-import SignUp from "./pages/SignUp"
+import Projects from "./pages/Projects";
+import Dashboard from "./pages/Dashboard";
+import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
 import Header from "./components/Header/Header";
 import FooterComp from "./components/Footer/FooterComp";
+import PrivateRoute from "./components/PrivateRoute";
+import AuthPrivateRoute from "./components/AuthPrivateRoute";
 
 export default function App() {
   return (
     <BrowserRouter>
-    <Header />
+      <Header />
       <Routes>
+        <Route element={<PrivateRoute />}>
+          <Route path="/dashboard" element={<Dashboard />} />
+        </Route>
+        <Route element={<AuthPrivateRoute />}>
+          <Route path="/sign-in" element={<SignIn />} />
+          <Route path="/Sign-up" element={<SignUp />} />
+        </Route>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
-        <Route path="/dashboard" element={ <Dashboard /> } />
-        <Route path="/projects" element={ <Projects /> } />
-        <Route path="/sign-in" element={ <SignIn /> } />
-        <Route path="/Sign-up" element={ <SignUp /> } />
-
-
+        <Route path="/projects" element={<Projects />} />
       </Routes>
       <FooterComp />
     </BrowserRouter>
